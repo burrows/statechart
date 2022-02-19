2863515bef85806e81dde7c1542b67c6ea3d8749
Corey Burrows <corey.burrows@gmail.com>
2022-02-19 07:54:38 -0600

    Add counter example.

--- examples/App.tsx
+++ examples/App.tsx
@@ -7,47 +7,130 @@ interface AppProps {}
 
 interface Ctx {
   count: number;
+  step: number;
 }
-type Evt = {type: 'x'};
+type Evt =
+  | {type: 'TOGGLE_ON_OFF'}
+  | {type: 'TOGGLE_AUTO'}
+  | {type: 'INCREMENT'}
+  | {type: 'DECREMENT'}
+  | {type: 'CHANGE_SPEED'};
 
-const sc = new Statechart<Ctx, Evt>({count: 0}, s => {
-  s.state('a', s => {
-    s.enter(ctx => ({context: {...ctx, count: ctx.count + 1}}));
-    s.on('x', '../b');
+class Timer {
+  timer?: number;
+
+  start(send: (e: Evt) => void): void {
+    this.timer = (setInterval(() => {
+      send({type: 'INCREMENT'});
+    }, 1000) as unknown) as number;
+  }
+
+  stop() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  }
+}
+
+const sc = new Statechart<Ctx, Evt>({count: 0, step: 0}, s => {
+  s.state('off', s => {
+    s.on('TOGGLE_ON_OFF', '../on');
   });
-  s.state('b', s => {
-    s.enter(ctx => ({context: {...ctx, count: ctx.count + 1}}));
-    s.on('x', '../a');
+
+  s.state('on', {concurrent: true, H: '*'}, s => {
+    s.state('mode', s => {
+      s.state('manual', s => {
+        s.on('TOGGLE_AUTO', '../auto');
+      });
+
+      s.state('auto', s => {
+        s.enter(() => ({activities: [new Timer()]}));
+        s.on('TOGGLE_AUTO', '../manual');
+      });
+
+      s.on('INCREMENT', ctx => ({
+        context: {...ctx, count: ctx.count + ctx.step},
+      }));
+      s.on('DECREMENT', ctx => ({
+        context: {...ctx, count: ctx.count - ctx.step},
+      }));
+    });
+
+    s.state('speed', s => {
+      s.state('slow', s => {
+        s.enter(ctx => ({context: {...ctx, step: 1}}));
+        s.on('CHANGE_SPEED', '../medium');
+      });
+      s.state('medium', s => {
+        s.enter(ctx => ({context: {...ctx, step: 10}}));
+        s.on('CHANGE_SPEED', '../fast');
+      });
+      s.state('fast', s => {
+        s.enter(ctx => ({context: {...ctx, step: 100}}));
+        s.on('CHANGE_SPEED', '../slow');
+      });
+    });
+
+    s.on('TOGGLE_ON_OFF', '../off');
   });
 });
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
   const [state, setState] = useState(sc.initialState);
 
-  // Create the counter (+1 every second).
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setCount(count + 1);
-      setState(state => sc.send(state, {type: 'x'}));
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+    for (const activity of state.activities.start) {
+      activity.start((evt: Evt) => {
+        setState(state => sc.send(state, evt));
+      });
+    }
+
+    for (const activity of state.activities.stop) {
+      activity.stop();
+    }
+  }, [state]);
+
+  const isOn = state.matches('/on');
+  const isAuto = state.matches('/on/mode/auto');
 
   // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <pre>{JSON.stringify(state.current.map(n => n.path))}</pre>
-        <pre>{JSON.stringify(state.context)}</pre>
-      </header>
+      <button
+        onClick={() => {
+          setState(state => sc.send(state, {type: 'TOGGLE_ON_OFF'}));
+        }}>
+        {isOn ? 'Turn Off' : 'Turn On'}
+      </button>
+      <button
+        disabled={!isOn || isAuto}
+        onClick={() => {
+          setState(state => sc.send(state, {type: 'INCREMENT'}));
+        }}>
+        Increment
+      </button>
+      <button
+        disabled={!isOn || isAuto}
+        onClick={() => {
+          setState(state => sc.send(state, {type: 'DECREMENT'}));
+        }}>
+        Decrement
+      </button>
+      <button
+        disabled={!isOn}
+        onClick={() => {
+          setState(state => sc.send(state, {type: 'TOGGLE_AUTO'}));
+        }}>
+        {isAuto ? 'Turn Auto Off' : 'Turn Auto On'}
+      </button>
+      <button
+        disabled={!isOn}
+        onClick={() => {
+          setState(state => sc.send(state, {type: 'CHANGE_SPEED'}));
+        }}>
+        Change Speed
+      </button>
+      <h1>{state.context.count}</h1>
+      <h6>Step: {state.context.step}</h6>
     </div>
   );
 }
