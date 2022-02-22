9d09953fa7c0cc854b4f59cb71786a1c9196f43c
Corey Burrows <corey.burrows@gmail.com>
2022-02-21 21:26:22 -0600

    Prepare for publishing.

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,6 @@ import Statechart from './Statechart';
 import Machine from './Machine';
 import State from './State';
 
-import {
-  Event as _Event,
-  EffectObj as _EffectObj,
-  EffectFn as _EffectFn,
-  Effect as _Effect,
-} from './types';
-
-export type Event = _Event;
-export type EffectObj<E> = _EffectObj<E>;
-export type EffectFn<E> = _EffectFn<E>;
-export type Effect<E> = _Effect<E>;
-
+export * from './types';
 export {Machine, State};
 export default Statechart;
