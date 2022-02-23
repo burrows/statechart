43b678df5db15860540807d25ca9f3de091603d2
Corey Burrows <corey.burrows@gmail.com>
2022-02-23 08:45:04 -0600

    Export Node class.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Statechart from './Statechart';
 import Machine from './Machine';
 import State from './State';
+import Node from './Node';
 
 export * from './types';
-export {Machine, State};
+export {Machine, State, Node};
 export default Statechart;
