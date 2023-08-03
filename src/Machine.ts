5c6902fff1053f674cbf5f904e7e44b7d7092dc0
Corey Burrows <corey.burrows@gmail.com>
2023-08-03 11:55:18 -0500

    Add observer and trace options to Machine constructor.

--- src/Machine.ts
+++ src/Machine.ts
@@ -3,6 +3,17 @@ import Node from './Node';
 import Statechart from './Statechart';
 import State from './State';
 
+const trace = (
+  event: Event | null,
+  from: State<any, any> | null,
+  to: State<any, any>,
+): void => {
+  const e = event ? event.type : '__init__';
+  const f = from ? JSON.stringify(from.current.map(n => n.path)) : '(null)';
+  const t = JSON.stringify(to.current.map(n => n.path));
+  console.info(`[${e}]: ${f} -> ${t}`);
+};
+
 /**
  * Provides a reference `Machine` class for maintaining the current state of a
  * statechart and executing side effects. It's perfectly valid choose to
@@ -10,23 +21,38 @@ import State from './State';
  */
 export default class Machine<C, E extends Event> {
   private state: State<C, E>;
+  private observer?: (state: State<C, E>) => void;
+  private trace: boolean;
 
-  constructor(private statechart: Statechart<C, E>) {
-    this.state = new State({context: statechart.initialContext});
+  constructor(
+    private statechart: Statechart<C, E>,
+    {
+      observer,
+      trace = false,
+    }: {observer?: (state: State<C, E>) => void; trace?: boolean} = {},
+  ) {
+    this.state = this.statechart.initialState;
+    this.observer = observer;
+    this.trace = trace;
   }
 
   start(): this {
-    this.state = this.statechart.initialState;
+    if (this.trace) trace(null, null, this.state);
+    this.observer?.(this.state);
     return this.exec(this.state);
   }
 
   stop(): this {
     this.state = this.statechart.stop(this.state);
+    this.observer?.(this.state);
     return this.exec(this.state);
   }
 
   send(event: E): this {
+    const from = this.state;
     this.state = this.statechart.send(this.state, event);
+    if (this.trace) trace(event, from, this.state);
+    this.observer?.(this.state);
     return this.exec(this.state);
   }
 
